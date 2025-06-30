d452a78c0ed330b8d3d08dfcc345433b83955bda
Anusha K N <natesananusha@gmail.com>
2025-07-01 04:59:16 +0530

    Add files via upload

---
+++ flutter_application_2/build/app/intermediates/cxx/Debug/01b645v2/logs/armeabi-v7a/configure_command.bat
@@ -0,0 +1,20 @@
+@echo off
+"C:\\Users\\nates\\AppData\\Local\\Android\\sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HC:\\Users\\nates\\flutter\\packages\\flutter_tools\\gradle\\src\\main\\scripts" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=21" ^
+  "-DANDROID_PLATFORM=android-21" ^
+  "-DANDROID_ABI=armeabi-v7a" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=armeabi-v7a" ^
+  "-DANDROID_NDK=C:\\Users\\nates\\AppData\\Local\\Android\\sdk\\ndk\\27.0.12077973" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\nates\\AppData\\Local\\Android\\sdk\\ndk\\27.0.12077973" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\nates\\AppData\\Local\\Android\\sdk\\ndk\\27.0.12077973\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\nates\\AppData\\Local\\Android\\sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\nates\\OneDrive\\Documents\\Desktop\\Learning\\flutter_application_2\\build\\app\\intermediates\\cxx\\Debug\\01b645v2\\obj\\armeabi-v7a" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\nates\\OneDrive\\Documents\\Desktop\\Learning\\flutter_application_2\\build\\app\\intermediates\\cxx\\Debug\\01b645v2\\obj\\armeabi-v7a" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BC:\\Users\\nates\\OneDrive\\Documents\\Desktop\\Learning\\flutter_application_2\\build\\.cxx\\Debug\\01b645v2\\armeabi-v7a" ^
+  -GNinja ^
+  -Wno-dev ^
+  --no-warn-unused-cli
